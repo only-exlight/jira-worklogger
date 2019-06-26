ad11da8193e309c77371d5a69083e417e760d266
Andrey Kernichniy <akernichniy@sinergo.ru>
2019-06-26 19:24:33 +0700

    build hoks

---
+++ webpack/pre-commit.config.js
@@ -0,0 +1,22 @@
+let nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    externals: {
+        child_process: 'child_process',
+        net: 'net'
+    },
+    output: {
+        path: `${__dirname}/build`,
+        filename: 'pre-commit.js'
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+};

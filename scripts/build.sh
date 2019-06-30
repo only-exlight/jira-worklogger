e554e3ad71a9474b504c853e07ef358f94897545
eXlight <kerpith@gmail.com>
2019-06-30 16:33:09 +0700

    change build, add run signature

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,25 @@
-rm -rf package
-webpack --config webpack.config.js
-cp src/final-package.json package
-mv package/final-package.json package/package.json
-cp LICENSE package
-cp README.md package
-npm publish package
+echo 'Delete old build...';
+rm -rf package;
+echo 'Ok!';
+#
+echo 'Build files...';
+webpack --config webpack.config.js;
+echo 'Ok!';
+#
+echo 'Copy: manifest, license, readme...';
+cp src/final-package.json package;
+mv package/final-package.json package/package.json;
+cp LICENSE package;
+cp README.md package;
+echo 'Ok!';
+#
+echo 'Add run signature to files...';
+cd package;
+echo "#!/usr/bin/env node\n$(cat jwl)" > jwl;
+echo "#!/usr/bin/env node\n$(cat use-jwl)" > use-jwl;
+cd ..;
+echo 'Ok!';
+#
+echo 'Public package...';
+npm publish package;
+echo 'Ok!';
--- src/utils/use-jwl.ts
+++ src/utils/use-jwl.ts
@@ -1,3 +1,4 @@
 import { APP_NAME, VERSION } from '../const/app-consts';
 
 process.stdout.write(`Use ${APP_NAME} ${VERSION} for this repo... \n`);
+process.stdout.write(`${__dirname}`);

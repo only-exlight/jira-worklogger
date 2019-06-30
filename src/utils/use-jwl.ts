c97a388ef8f7fc1b2b84c6f0ffdaf6ee7528b2dc
eXlight <kerpith@gmail.com>
2019-06-30 19:55:26 +0700

    use jwl util

--- src/utils/use-jwl.ts
+++ src/utils/use-jwl.ts
@@ -1,4 +1,25 @@
 import { APP_NAME, VERSION } from '../const/app-consts';
+import { existsSync, appendFileSync, writeFileSync } from 'fs';
 
-process.stdout.write(`Use ${APP_NAME} ${VERSION} for this repo... \n`);
-process.stdout.write(`${__dirname}`);
+process.stdout.write(`Use ${APP_NAME} ${VERSION} for this repository... \n`);
+
+let hoooooksPath = __dirname.split('/');
+hoooooksPath.length--;
+
+const commitMsgPath = './.git/hooks/commit-msg'
+const commitMsg = `node ${hoooooksPath}/hoooooks/commit-msg.js $1`;
+
+const postCheckoutPath = './.git/hooks/post-checkout';
+const postCheckout = `node ${hoooooksPath}/hoooooks/post-checkout.ts`;
+
+if (existsSync('./.git')) {
+    if (existsSync('./.git/hooks')) {
+        appendFileSync(commitMsgPath, commitMsg);
+        appendFileSync(postCheckoutPath, postCheckout);
+    } else {
+        writeFileSync(commitMsgPath, commitMsg);
+        writeFileSync(postCheckoutPath, postCheckout);
+    }
+} else {
+    process.stdout.write('GIT repository not found!');
+}

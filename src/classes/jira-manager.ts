38905b0b8e034e4a02d7a6dfe902bbe3e811366a
eXlight <kerpith@gmail.com>
2019-07-01 01:59:30 +0700

    add jira client

--- src/classes/jira-manager.ts
+++ src/classes/jira-manager.ts
@@ -1,24 +1,17 @@
-import axios from 'axios';
-import { AxiosResponse, AxiosError } from 'axios';
-import { JIRA_DOMAIN_V2, JIRA_DOMAIN } from '../const/jira';
-import { IJiraWorkLog, IJiraSession, IJiraLogin, IReport } from '../interfaces/jira';
+import { IJiraSession, IReport, IJiraWorkLog } from '../interfaces/jira';
 import { stdinQuestion, writeSeparator } from '../functions/exec';
 import { Bar, Presets } from 'cli-progress';
 import { bgBlue } from 'colors';
+import JiraClient from 'jira-client';
 
 export class JiraManager {
-    private session: IJiraSession;
-
-    public async login() {
-        writeSeparator();
-        let sucess = false;
-        process.stdout.write('Please auth to JIRA: \n');
-        writeSeparator();
-        const login = await stdinQuestion('Login: \n');
-        const password = await stdinQuestion('Password: \n');
-        sucess = true // await this.jiraLogin(login, password);
-        !sucess ? await this.login() : process.stdout.write('Success! \n');
-    }
+    private jiraClient = new JiraClient({
+        protocol: 'https',
+        apiVersion: '2',
+        host: 'http://jira.ntrlab.ru',
+        username: 'akernichniy',
+        password: '2016jiraMinsk',
+    });
 
     public async jiraWorkLogMass(reports: IReport[]) {
         process.stdout.write('Please wait for the end logging work process to JIRA system...\n\n');
@@ -26,7 +19,7 @@ export class JiraManager {
         progres.start(reports.length, 0);
         let progressState = 0;
         reports.forEach(async r => {
-            await this.jiraLogWork(r.taskName, r.report);
+            await this.jiraClient.addWorklog(r.taskName, r.report);
             progressState++;
             progres.update(progressState);
             if (progressState === reports.length) {
@@ -36,31 +29,4 @@ export class JiraManager {
         });
     }
 
-    public async jiraLogWork(task: string, info: IJiraWorkLog): Promise<boolean> {
-        try {
-            /*await axios.post(`${JIRA_DOMAIN_V2}/${task}/worklog`, info, {
-                headers: { Cookie: `${this.session.name}=${this.session.value};` }
-            });*/
-            return true;
-        } catch (error) {
-            console.log(error);
-            return false;
-        }
-    }
-
-    private async jiraLogin(user: string, psw: string): Promise<boolean> {
-        try {
-            const answ: AxiosResponse<IJiraLogin> = await axios.post(`${JIRA_DOMAIN}/rest/auth/1/session`, {
-                username: user,
-                password: psw
-            });
-            this.session = answ.data.session;
-            return true;
-        } catch (err) {
-            const e: AxiosError = err;
-            console.error(e.message);
-            // write message here
-            return false;
-        }
-    }
 }
